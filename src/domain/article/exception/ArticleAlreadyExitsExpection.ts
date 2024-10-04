3e27848f7f82de415769adabac516249991a8490
hy2n <github1@ncloud.sbs>
2024-10-05 05:48:21 +0900

    chore : 필요없는 예외 삭제

--- src/domain/article/exception/ArticleAlreadyExitsExpection.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
-
-export class ArticleAlreadyExistsException extends HttpException {
-    constructor() {
-        super('Already Exits Article',HttpStatus.CONFLICT);
-    }
-}
