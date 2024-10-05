8c5714ad5eb2e8763aaa0d1e193c2acfee90e005
hy2n <github1@ncloud.sbs>
2024-10-06 05:58:44 +0900

    chore : 예외 내용 수정

--- src/domain/category/exception/CategoryNotFoundException.ts
+++ src/domain/category/exception/CategoryNotFoundException.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from "@nestjs/common";
 
 export class CategoryNotFoundException extends HttpException {
     constructor() {
-        super('Requested Category Not Found',HttpStatus.NOT_FOUND);
+        super('Category Not Found',HttpStatus.NOT_FOUND);
     }
 }
