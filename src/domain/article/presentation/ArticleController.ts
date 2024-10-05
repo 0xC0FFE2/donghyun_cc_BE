9b337e9df3a06a8fcf2df7345eab8f333733bf44
hy2n <github1@ncloud.sbs>
2024-10-06 06:39:39 +0900

    feature : Article Controller 작성

--- src/domain/article/presentation/ArticleController.ts
+++ src/domain/article/presentation/ArticleController.ts
@@ -1,14 +1,48 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { ArticleService } from '../service/ArticleCreateService';
-import { CreateArticle } from './dto/create-article.dto';
-import { ArticleResponse } from './dto/article-response.dto';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { ArticleCreateService } from '../service/ArticleCreateService';
+import { ArticleReadService } from '../service/ArticleReadService';
+import { ArticleUpdateService } from '../service/ArticleUpdateService';
+import { ArticleDeleteService } from '../service/ArticleDeleteService';
+import { CreateArticleRequest } from '../presentation/dto/request/CreateArticleRequest';
+import { UpdateArticleRequest } from '../presentation/dto/request/UpdateArticleRequest';
+import { Article } from '../domain/Article.entity';
 
 @Controller('articles')
 export class ArticleController {
-    constructor(private readonly articleService: ArticleService) { }
+    constructor(
+        private readonly articleCreateService: ArticleCreateService,
+        private readonly articleReadService: ArticleReadService,
+        private readonly articleUpdateService: ArticleUpdateService,
+        private readonly articleDeleteService: ArticleDeleteService,
+    ) { }
 
     @Post()
-    async create(@Body() createArticleDto: CreateArticle): Promise<ArticleResponse> {
-        return this.articleService.createArticle(createArticleDto);
+    async createArticle(@Body() createArticleDto: CreateArticleRequest): Promise<Article> {
+        return this.articleCreateService.createArticle(createArticleDto);
+    }
+
+    @Get(':id')
+    async getArticleById(@Param('id') articleId: string): Promise<Article> {
+        return this.articleReadService.readArticle(articleId);
+    }
+
+    @Get()
+    async getArticles(
+        @Query('page') page: number = 1
+    ): Promise<{ articles: Article[], totalPage: number }> {
+        return this.articleReadService.getArticle(page);
+    }
+
+    @Put(':id')
+    async updateArticle(
+        @Param('id') articleId: string,
+        @Body() updateArticleDto: UpdateArticleRequest
+    ): Promise<Article> {
+        return this.articleUpdateService.updateArticle(articleId, updateArticleDto);
+    }
+
+    @Delete(':id')
+    async deleteArticle(@Param('id') articleId: string): Promise<boolean> {
+        return this.articleDeleteService.deleteArticle(articleId);
     }
 }
