daa18314e0e56441c6efd5545907a72161d0926e
hy2n <github1@ncloud.sbs>
2024-10-06 14:20:11 +0900

    feature : typeORM 연결

--- src/domain/AppModule.ts
+++ src/domain/AppModule.ts
@@ -1,8 +1,14 @@
-// app.module.ts
 import { Module } from '@nestjs/common';
 import { ArticleModule } from './article/ArticleModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ArticleModule],
+  imports: [
+    TypeOrmModule.forRoot({
+        type:'sqlite',
+        database:'database',
+        entities: [__dirname + '/**/*.entity{.ts,.js}']
+    })
+    ,ArticleModule],
 })
 export class AppModule {}
--- src/domain/article/ArticleModule.ts
+++ src/domain/article/ArticleModule.ts
@@ -4,9 +4,14 @@ import { ArticleReadService } from './service/ArticleReadService';
 import { ArticleUpdateService } from './service/ArticleUpdateService';
 import { ArticleDeleteService } from './service/ArticleDeleteService';
 import { ArticleController } from './presentation/ArticleController';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from './domain/Article.entity';
+import { ArticleRepository } from './domain/repository/ArticleRepository';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Article,ArticleRepository]),
+  ],
   controllers: [ArticleController],
   providers: [ArticleCreateService,ArticleReadService,ArticleUpdateService,ArticleDeleteService],
 })
