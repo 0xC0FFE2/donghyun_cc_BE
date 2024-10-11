0046fb9d098a20152bbddb5e53b076c9b235e553
hy2n <github1@ncloud.sbs>
2024-10-12 00:26:47 +0900

    docs : 주석 추가

--- src/domain/visitor/presentation/middleware/VisitorAddMiddleware.ts
+++ src/domain/visitor/presentation/middleware/VisitorAddMiddleware.ts
@@ -7,7 +7,7 @@ export class VisitorAddMiddleware implements NestMiddleware {
     constructor(private readonly visitorAddService: VisitorAddService) { }
 
     async use(req: Request, res: Response, next: NextFunction) {
-        const realIp = req.headers['x-forwarded-for'] || req.ip;
+        const realIp = req.headers['x-forwarded-for'] || req.ip; //cloudflare proxy 대응 headers 추가
         const ip = Array.isArray(realIp) ? realIp[0] : realIp;
         await this.visitorAddService.AddVisitor(ip);
         next();
