4fb3232deb71b95aa008817398876b88194cbb2d
0xC0FFE2 <github1@ncloud.sbs>
2025-02-11 19:20:02 +0900

    fix : HttpModule 의존성 추가

--- src/global/auth/AuthModule.ts
+++ src/global/auth/AuthModule.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthStrategy } from './strategies/AuthStrategy';
 import { AdminGuard } from './guard/AdminGuard';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+    imports: [HttpModule],
     providers: [AuthStrategy, AdminGuard],
     exports: [AuthStrategy,AdminGuard],
 })
