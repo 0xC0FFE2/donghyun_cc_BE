1cd44146679067f5cb3778adf54af7cbab7cfe02
0xC0FFE2 <github1@ncloud.sbs>
2025-02-16 05:43:01 +0900

    chore : 코드개선 (방문자수 안올라가는 버그 fix)

--- src/domain/visitor/presentation/middleware/VisitorAddMiddleware.ts
+++ src/domain/visitor/presentation/middleware/VisitorAddMiddleware.ts
@@ -4,12 +4,15 @@ import { VisitorAddService } from '../../service/VisitorAddService';
 
 @Injectable()
 export class VisitorAddMiddleware implements NestMiddleware {
-    constructor(private readonly visitorAddService: VisitorAddService) { }
+  constructor(private readonly visitorAddService: VisitorAddService) {}
 
-    async use(req: Request, res: Response, next: NextFunction) {
-        const realIp = req.headers['x-forwarded-for'] || req.ip; //cloudflare proxy 대응 headers 추가
-        const ip = Array.isArray(realIp) ? realIp[0] : realIp;
-        await this.visitorAddService.AddVisitor(ip);
-        next();
-    }
+  async use(req: Request, res: Response, next: NextFunction) {
+    const realIp =
+      req.headers['cf-connecting-ip'] ||
+      req.headers['x-forwarded-for'] ||
+      req.ip;
+    const ip = Array.isArray(realIp) ? realIp[0] : realIp;
+    await this.visitorAddService.AddVisitor(ip);
+    next();
+  }
 }
--- src/domain/visitor/service/VisitorAutoUpdateService.ts
+++ src/domain/visitor/service/VisitorAutoUpdateService.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { VisitorRepository } from '../domain/repository/VisitorRepository';
-import { BlogUpdateService } from 'src/domain/blog/service/BlogUpdateService';
+import { BlogUpdateService } from '../../blog/service/BlogUpdateService';
 
 @Injectable()
 export class VisitorAutoUpdateService {
-
     constructor(
         private readonly visitorRepository: VisitorRepository,
         private readonly blogUpdateService: BlogUpdateService
-    ) { }
+    ) {}
 
     @Cron('0 0 * * *')
     async handleCron() {
         const visitorCount = await this.visitorRepository.getVisitorCountByDate();
-        this.addTotalVisitor(visitorCount);
+        await this.addTotalVisitor(visitorCount);
     }
 
-    addTotalVisitor(count: number): void {
+    async addTotalVisitor(count: number): Promise<void> {
         console.log(`${count} UPDATED`);
-        this.blogUpdateService.addTotalvisitor(count);
-        this.visitorRepository.deleteVisitorHistory();
+        await this.blogUpdateService.addTotalvisitor(count);
+        await this.visitorRepository.deleteVisitorHistory();
     }
 }
